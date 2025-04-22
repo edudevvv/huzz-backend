bdeafeeb2d450f02d1b86ecabb6e4c6a77c3e11a
pgwl <154462608+edudevvv@users.noreply.github.com>
2025-04-22 13:47:08 -0300

    :)

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,7 @@ export const envConfig = {
   clientSecret: process.env.CLIENT_SECRET,
   callback: process.env.CALLBACK,
 
-  keySecret: process.env.SECRET_KEY as string,
+  keySecret: process.env.SECRET_KEY,
   webhook: process.env.WEBHOOK,
 
   adminSecret: process.env.SECRET_ADMIN
